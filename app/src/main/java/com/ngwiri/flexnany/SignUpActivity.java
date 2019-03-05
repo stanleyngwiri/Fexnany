e8124049e32fb1c86b60cd803a77aeec3503d6ee
Wess58 <wesleymbate@gmail.com>
2019-03-05 18:05:28 +0300

    Binded views from SignUp xml file

--- app/src/main/java/com/ngwiri/flexnany/SignUpActivity.java
+++ app/src/main/java/com/ngwiri/flexnany/SignUpActivity.java
@@ -1,13 +1,21 @@
 package com.ngwiri.flexnany;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
