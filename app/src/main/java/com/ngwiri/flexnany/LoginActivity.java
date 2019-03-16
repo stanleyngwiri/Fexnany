6e4a73b0059ce0e9f4e33d5a5fe83b997911bf83
Wess58 <wesleymbate@gmail.com>
2019-03-16 12:48:09 +0300

    created validation method

--- app/src/main/java/com/ngwiri/flexnany/LoginActivity.java
+++ app/src/main/java/com/ngwiri/flexnany/LoginActivity.java
@@ -4,17 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @BindView(R.id.loginEmail) TextView mLoginEmail;
+    private static View view;
+    private static Animation shakeAnimation;
+
+    @BindView(R.id.loginLayout) LinearLayout mLoginLayout;
     @BindView(R.id.loginbuton) Button mLoginbuton;
     @BindView(R.id.SignUp_Text) TextView mSignUp_Text;
+    @BindView(R.id.loginEmail) EditText mLoginEmail;
+    @BindView(R.id.password) EditText mPassword;
 
 
     @Override
@@ -27,18 +40,57 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mLoginbuton.setOnClickListener(this);
         mSignUp_Text.setOnClickListener(this);
 
+         shakeAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
+
+
     }
 
     @Override
     public void onClick(View v) {
-        if (v == mLoginbuton){
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
+        if (v == mLoginbuton) {
+//            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//            startActivity(intent);
+
+            checkValidation();
+
+
+
         }
 
         if (v == mSignUp_Text){
             Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
             startActivity(intent);
         }
+    }
+
+    // Check Validation before login
+    private void checkValidation () {
+        // Get email id and password
+        String getEmailId = mLoginEmail.getText().toString();
+        String getPassword = mPassword.getText().toString();
+
+        // Check patter for email id
+        Pattern p = Pattern.compile(Utils.regEx);
+
+        Matcher m = p.matcher(getEmailId);
+
+        // Check for both field is empty or not
+
+        if (getEmailId.equals("") || getEmailId.length() == 0
+                || getPassword.equals("") || getPassword.length() == 0) {
+            mLoginLayout.startAnimation(shakeAnimation);
+            new CustomToast().Show_Toast(getApplicationContext(), view,
+                    "Enter both credentials.");
+
+        }
+        // Check if email id is valid or not
+        else if (!m.find())
+            new CustomToast().Show_Toast(getApplicationContext(), view,
+                    "Your Email Id is Invalid.");
+            // Else do login and do your stuff
+        else
+            Toast.makeText(getApplicationContext(), "Do Login.", Toast.LENGTH_SHORT)
+                    .show();
+
     }
 }
