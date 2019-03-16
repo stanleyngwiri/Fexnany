75d28e2fa0e9ef9b612616322e88e4e38f8e5693
Wess58 <wesleymbate@gmail.com>
2019-03-16 12:27:00 +0300

    Utils.java for regex for email validation

---
+++ app/src/main/java/com/ngwiri/flexnany/Utils.java
@@ -0,0 +1,9 @@
+package com.ngwiri.flexnany;
+
+public class Utils {
+    //Email Validation pattern
+    public static final String regEx = "\"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$\"";
+
+    public static final String Login_Activity = "Login_Activity";
+    public static final String SignUp_Activity = "SignUp_Activity";
+}
