c795c5bc715c58f4b877a83bdf6457e8f9a2d6a3
Wess58 <wesleymbate@gmail.com>
2019-03-17 19:23:58 +0300

    linked App to Firebase and added Login function

--- app/src/main/java/com/ngwiri/flexnany/LoginActivity.java
+++ app/src/main/java/com/ngwiri/flexnany/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.ngwiri.flexnany;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
@@ -12,6 +14,12 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,6 +31,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private static View view;
     private static Animation shakeAnimation;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private ProgressDialog progressDialog;
+
+
 
     boolean doubleBackToExitPressedOnce = false;
 
@@ -38,6 +52,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        createAuthProgressDialog();
+        addAuth();
+
 
 
         mLoginbuton.setOnClickListener(this);
@@ -82,7 +101,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if (getEmailId.equals("") || getEmailId.length() == 0
                 || getPassword.equals("") || getPassword.length() == 0) {
             mLoginLayout.startAnimation(shakeAnimation);
-            new CustomToast().Show_Toast(getApplicationContext(), view,
+            new CustomToast().Show_Toast(getBaseContext(), view,
                     "Enter both credentials.");
 
         }
@@ -97,6 +116,50 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
+    //<--- PROGRESSDIALOG START
+    //setCancelable() to "false" so that users cannot close the dialog manually.
+//    private void createAuthProgressDialog() {
+//        progressDialog = new ProgressDialog(this);
+//        progressDialog.setTitle("Loading ...");
+//        progressDialog.setMessage("Authenticating in Progress...");
+//        progressDialog.setCancelable(false);
+//
+//    }
+    //PROGRESSDIALOG END --->
+
+
+
+    public  void login(String email,String password){
+        progressDialog.show();
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                if(task.isSuccessful()){
+                    Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Authentication Failed",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+//    public void addAuth(){
+//        authStateListener=new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+//                if(user!=null){
+//                    Intent intent=new Intent( LoginActivity.this, MainActivity.class);
+//                    startActivity(intent);
+//                }
+//            }
+//        };
+//    }
+
     @Override
     public void onBackPressed() {
         if (doubleBackToExitPressedOnce) {
@@ -105,7 +168,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
 
         this.doubleBackToExitPressedOnce = true;
-        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+        Toast.makeText( LoginActivity.this, "Click BACK again to exit", Toast.LENGTH_SHORT).show();
 
         new Handler().postDelayed(new Runnable() {
 
@@ -115,4 +178,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         }, 2000);
     }
+
+//    @Override
+//    protected  void onStart(){
+//        super.onStart();
+//        FirebaseUser user=mAuth.getCurrentUser();
+//        if(user!=null){
+//            mAuth.addAuthStateListener(authStateListener);
+//        }
+//    }
+//
+//    @Override
+//    protected  void  onStop(){
+//        super.onStop();
+//        if(authStateListener!=null){
+//            mAuth.removeAuthStateListener(authStateListener);
+//        }
+//    }
 }
