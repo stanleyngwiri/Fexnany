31d85c2f70a5cf3ebfbed99c051dcefbc1cd4b82
Wess58 <wesleymbate@gmail.com>
2019-03-20 15:02:51 +0300

    added Access State Permissions

---
+++ app/src/main/java/com/ngwiri/flexnany/Network.java
@@ -0,0 +1,36 @@
+package com.ngwiri.flexnany;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+public class Network {
+    private static final String TAG = Network.class.getSimpleName();
+
+    public static boolean isInternetAvailable(Context context)
+    {
+        NetworkInfo info = ((ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+
+        if (info == null)
+        {
+            Log.d(TAG,"no internet connection");
+            return false;
+        }
+        else
+        {
+            if(info.isConnected())
+            {
+                Log.d(TAG," internet connection available...");
+                return true;
+            }
+            else
+            {
+                Log.d(TAG," internet connection");
+                return true;
+            }
+
+        }
+    }
+}
