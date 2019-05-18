2b4834b5c90541ecdf666a94cdd8647cf0a61e7f
Wess58 <wesleymbate@gmail.com>
2019-05-18 13:49:04 +0300

    added some more essential attributes to display

--- app/src/main/java/com/ngwiri/flexnany/adapters/maidsListAdapter.java
+++ app/src/main/java/com/ngwiri/flexnany/adapters/maidsListAdapter.java
@@ -1,7 +1,6 @@
 package com.ngwiri.flexnany.adapters;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,34 +27,18 @@ public class maidsListAdapter extends RecyclerView.Adapter<maidsListAdapter.maid
         mMaids = maids;
     }
 
-    @Override
-    public maidsListAdapter.maidsViewHolder onCreateViewHolder( ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.maids_list_item, viewGroup, false);
-        maidsViewHolder viewHolder = new maidsViewHolder(view);
-        return viewHolder;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull maidsViewHolder maidsViewHolder, int i) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
-
     public class maidsViewHolder extends RecyclerView.ViewHolder{
 
         @BindView(R.id.maidPlaceholderImage) CircleImageView mMaidPlaceholderImage;
-        @BindView(R.id.maidPhoneNumber) TextView mMaidPhoneNumber;
+        @BindView(R.id.maidName) TextView mMaidName;
+        @BindView(R.id.maidMsisdn) TextView mMaidMsisdn;
         @BindView(R.id.maidDescription) TextView mMaidDescription;
         @BindView(R.id.maidAddress) TextView mMaidAddress;
         @BindView(R.id.maidRating) TextView mMaidRating;
+        @BindView(R.id.maidExperience) TextView mMaidExperience;
+        @BindView(R.id.maidAge) TextView mMaidAge;
         @BindView(R.id.maidServices) TextView mMaidServices;
         @BindView(R.id.maidStatus) TextView mMaidStatus;
-
 
         private Context mContext;
 
@@ -66,9 +49,43 @@ public class maidsListAdapter extends RecyclerView.Adapter<maidsListAdapter.maid
 
         }
 
-
-
+        // bindMaids() set the contents of the layout's TextViews to the attributes of a specific maid
+        public void bindMaids(Maids maids) {
+            mMaidName.setText(maids.getmName());
+            mMaidMsisdn.setText(maids.getmMsisdn());
+            mMaidDescription.setText(maids.getmDescription());
+            mMaidAddress.setText(maids.getmAddress());
+            mMaidRating.setText(maids.getmRating() + "/5");
+            mMaidExperience.setText(maids.getmExperience());
+            mMaidAge.setText(maids.getmAge() + "Years");
+            mMaidServices.setText(maids.getmServices());
+            mMaidStatus.setText(maids.getmStatus());
+        }
     }
+
+
+    // method inflates the layout, and creates the ViewHolder object required from the adapter
+    @Override
+    public maidsListAdapter.maidsViewHolder onCreateViewHolder( ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.maids_list_item, viewGroup, false);
+        maidsViewHolder viewHolder = new maidsViewHolder(view);
+        return viewHolder;
+    }
+
+    //updates the contents of the ItemView
+    @Override
+    public void onBindViewHolder( maidsListAdapter.maidsViewHolder holder, int i) {
+        holder.bindMaids(mMaids.get(i));
+    }
+
+    //sets the number of items the adapter will display.
+    @Override
+    public int getItemCount() {
+        return mMaids.size();
+    }
+
+
+
 
 
 
