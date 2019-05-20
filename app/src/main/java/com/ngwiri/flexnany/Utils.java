179b1484d96fcc5453556066302a1c2a7403cf16
Wess58 <wesleymbate@gmail.com>
2019-05-20 14:06:38 +0300

    removed commented code

--- app/src/main/java/com/ngwiri/flexnany/Utils.java
+++ app/src/main/java/com/ngwiri/flexnany/Utils.java
@@ -4,6 +4,4 @@ public class Utils {
     //Email Validation pattern
     public static final String regEx = "^[_A-Za-z]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
-//    public static final String LoginActivity = "Login_Activity";
-//    public static final String SignUp_Activity = "SignUp_Activity";
 }
