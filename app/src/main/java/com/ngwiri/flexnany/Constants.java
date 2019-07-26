9d544566e1160664ac4af1e41df9ba4d4531cd3f
Wess58 <wesleymbate@gmail.com>
2019-07-26 11:28:02 +0300

    moved raw URL intp constants file

--- app/src/main/java/com/ngwiri/flexnany/Constants.java
+++ app/src/main/java/com/ngwiri/flexnany/Constants.java
@@ -3,5 +3,7 @@ package com.ngwiri.flexnany;
 public class Constants {
 
     public static final String MAIDS_BASE_URL = "http://51.15.233.87/maids/get-maids.php";
+    public static final String ABOUT_BASE_URL = "https://stanleyngwiri.github.io/ABOUT/";
+    public static final String ADS_POLICY_BASE_URL = "https://stanleyngwiri.github.io/POLICY/";
 
 }
